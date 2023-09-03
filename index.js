300d6dff1830f19011857717ff1cb863fee5ba07
MonishPro <mayekarmonish875@gmail.com>
2023-09-03 12:05:55 +0530

    Host

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path=require("path");
 
 let port=80;
 
-app.use(express.urlencoded());
+// app.use(express.urlencoded());
 app.set("view engine","pug");
 app.set("/static",express.static("static"));
 app.set("views",path.join(__dirname,"template"));
